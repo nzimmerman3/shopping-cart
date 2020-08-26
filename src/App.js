779cb4ecaec15e44b6c1784300fdfd956fc4a0db
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2020-08-26 15:30:09 -0700

    Changed website theme

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
           </div>
         </div>
       </div>
+      <footer>
+        Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </footer>
     </div>
   );
 }
