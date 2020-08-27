1acc08e9b6f3f2bbe544f3044af7b9a7adeff6f2
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2020-08-27 14:07:50 -0700

    Added filter functionality

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,23 +2,47 @@ import React from 'react'
 import data from "../data.json"
 import { Link, BrowserRouter } from 'react-router-dom';
 import {formatCurrency, formatDescription} from "../util"
+import FilterBar from './FilterBar'
 
 class Items extends React.Component {
     constructor() {
         super()
         this.state = {
             products: data,
-            size: "",
-            sort: ""
+            sort: "",
+            filter: ""
+        }
+        this.filterBy = this.filterBy.bind(this)
+    }
+     filterBy(event) {
+        // return this.state.products.filter(item => {item.type === type})
+        console.log(event.target.value)
+        if (event.target.value === "") {
+            this.setState({ filter: event.target.value, products: data})
+        }
+        else {
+            this.setState({
+                filter: event.target.value,
+                products: data.filter(
+                    (product) => product.type === event.target.value
+                )
+            })
         }
     }
+
     render() {
         return (
             <div>
+                <FilterBar 
+                    count={this.state.products.length}
+                    sort={this.state.sort}
+                    filter={this.state.filter}
+                    filterBy={this.filterBy}
+                />
                 <div className="row" style={{}}>
                     {this.state.products.map(item => {
                         return (
-                            <div className="col col-sm-3 d-flex">
+                            <div className="col col-sm-3 d-flex" style={{"min-width": "25%"}}>
                                 <div className="card">
                                     <div className="clickable">
                                         <img className="card-img-top" src={item.image} alt={item.name}/>
