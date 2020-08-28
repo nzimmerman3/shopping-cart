5f8fd2f0aaa60a4f029336ed122af93c82e40637
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2020-08-28 14:49:05 -0700

    Updated logo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Items />
       </div>
       <footer className="footer">
-        <div className="text-muted" style={{"paddingBottom": "0px", "margin-bottom": "0"}}>
+        <div className="text-muted" style={{"paddingBottom": "0px", "marginBottom": "0"}}>
           Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
         </div>
       </footer>
