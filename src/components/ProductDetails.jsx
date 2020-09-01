19f624eed82aab763fcea3714ec31ba9fbbe46e4
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2020-09-01 11:00:30 -0700

    Changed modal size

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -3,9 +3,24 @@ import Modal from "react-modal";
 import { formatCurrency } from "../util";
 
 export default function ProductDetails(props) {
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "80%",
+    },
+  };
   return (
     <div>
-      <Modal isOpen={true} onRequestClose={props.closeModal}>
+      <Modal
+        isOpen={true}
+        onRequestClose={props.closeModal}
+        style={customStyles}
+      >
         <h1>{props.product.name}</h1>
         <img
           className="modal-img"
